47ce79dd6d64b6903693c75b7ff91120d948a9c5
Vikash Maheshwari <vikasmaheshwari6267@gmail.com>
2025-02-13 18:42:04 +0530

    url

--- frontend/src/app/layout.jsx
+++ frontend/src/app/layout.jsx
@@ -1,6 +1,40 @@
 import "./globals.css";
 import Wrapper from "./wrapper";
 
+
+
+export const generateMetadata = () => {
+  return {
+    title: "Aditya Wedding & Event Planner",
+    description:
+      "Aditya Wedding & Event Planner",
+    keywords:
+      "Aditya Wedding & Event Planner",
+    // url: "https://www.tradegyan.co/",
+    image: "/android-chrome-192x192.png",
+
+    // Open Graph Tags
+    openGraph: {
+      type: "website",
+      // url: "https://www.tradegyan.co/",
+      title: "Aditya Wedding & Event Planner",
+      description:
+        "Aditya Wedding & Event Planner",
+      image: "/android-chrome-192x192.png",
+    },
+
+    // Twitter Tags
+    twitter: {
+      card: "summary_large_image",
+      title: "Aditya Wedding & Event Planner",
+      description:
+        "Aditya Wedding & Event Planner",
+      image: "/android-chrome-192x192.png",
+    },
+  };
+};
+
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
